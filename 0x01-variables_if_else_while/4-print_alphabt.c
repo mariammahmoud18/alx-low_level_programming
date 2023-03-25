227bdcbba6197aadee84f1d11305adb1af0ba923
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-03-25 12:59:37 -0700

    lol

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,14 @@
 */
 int main(void)
 {
-    char c = 97;
-    while (c <= 122)
-    {
-        if (c != 101 && c != 113)
-            putchar(c);
-        c++;
-    }
-   
-    putchar('\n');
-	return (0);
+char c = 97;
+
+while (c <= 122)
+{
+if (c != 101 && c != 113)
+    putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
 }
