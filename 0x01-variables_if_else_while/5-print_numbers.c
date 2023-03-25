cd438f4a46729a5c0a8871148a237769f58ac7ee
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-03-25 12:49:34 -0700

    lol

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,15 +7,17 @@
  *
  * Return: Always 0.
 */
+
 int main(void)
 {
-    int n = 0;
-    while (n <= 9)
-    {
-        printf("%d",n);
-        n++;
-    }
+        int n = 0;
+
+        while (n <= 9)
+        {
+                printf("%d",n);
+                n++;
+        }
    
-    putchar('\n');
-	return (0);
+        putchar('\n');
+        return (0);
 }
