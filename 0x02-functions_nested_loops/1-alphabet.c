3d1f6c5fcb1574401d1fa64ab1d6e909ff1fb239
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-03-26 11:41:06 -0700

    added two function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabet letters in lowercase.
+ *
+ * Return: 1 on sucess.
+*/
+void print_alphabet()
+{
+char c = 97;
+
+while (c <= 122)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print all alphabet letters in lowercase for 10 times.
+ *
+ * Return: 1 on sucess.
+*/
+void print_alphabet_x10(void)
+{
+char c = 97;
+int i = 0;
+
+while(i<10)
+{
+while (c <= 122)
+{
+_putchar(c);
+}
+i++;
+}
+_putchar('\n');
+}
