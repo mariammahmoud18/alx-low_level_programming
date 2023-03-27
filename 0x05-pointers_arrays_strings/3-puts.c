6273274267e3d925508d66ee7fe77dcaa3e7270a
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-03-27 12:23:41 -0700

    added Task 2 to 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts - prints a string followed by a new line.
+ * @str: string to be printed out.
+ * Return: 1 if successful.
+*/
+
+void _puts(char *str)
+{
+puts(str);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse.
+ * @s: string to be printed out.
+ * Return: 1 if successful.
+*/
+
+void print_rev(char *s)
+{
+int len = strlen(s);
+int i;
+
+for (i = len-1; i >= 0; i--)
+{
+putchar(s[i]);
+}
+putchar('\n');
+}
