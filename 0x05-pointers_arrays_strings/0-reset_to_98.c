1b0dc2df4bf0c54df897e2074f500d68b7346e09
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-03-27 11:36:12 -0700

    simple pointer function in C

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - sets a number to 98.
+ * @n: number to be set to 98.
+ * Return: 1 if successfuly.
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Dealing with pointers and arrays in C
