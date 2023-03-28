9220b6dbbf1eef9bd4c450dd1db5bf2e899b0d3f
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-03-27 18:08:56 -0700

    added task 8 & 9

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array - prints n elements of array.
+ * @a: array to be printed.
+ * @n: number of elements to be printed.
+ * Return: 1 if successful.
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n ; i++)
+{
+if (i == n - 1)
+printf("%d", a[i]);
+else
+printf("%d, ", a[i]);
+}
+}
+
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _strcpy - copies a string into another.
+ * @des: string to be copied into.
+ * @src: string to be copied.
+ * Return: pointer to des.
+*/
+
+char *_strcpy(char *des, char *src)
+{
+strcpy(des,src);
+return(des);
+}
