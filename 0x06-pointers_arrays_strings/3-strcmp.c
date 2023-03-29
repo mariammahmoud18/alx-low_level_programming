058ed2d93334d568171aadde7135195c9f2fb860
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-03-29 12:52:49 -0700

    adding strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,10 @@
-#include <main.h>
+#include "main.h"
 #include <string.h>
-
 /**
-*_strcmp - a function that compares two strings.
-*@s1: first string to be compared.
-*@s2: second string to be compared.
-*Return: comaprison result. 
+* _strcmp - a function that compares two strings.
+* @s1: first string to be compared.
+* @s2: second string to be compared.
+* Return: comaprison result. 
 */
 
 int _strcmp(char *s1, char *s2)
