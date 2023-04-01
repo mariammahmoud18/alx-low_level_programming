0699e0d5cd1555e4223c5c5f29429571e25cd895
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-01 00:55:30 -0700

    fixing a task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - function to print a sentence.
+ *
+ * Return: 0 if sucessful.
+ */
+
 int main(void)
 {
-   	 printf("with proper grammar, but the outcome is a piece of art,\n");
-   	 return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
