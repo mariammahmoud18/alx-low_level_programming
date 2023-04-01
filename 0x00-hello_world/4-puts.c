c2a73862768991bfaf6a8017b2226247fe5024ba
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-01 01:01:54 -0700

    fixing a task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - prints a sentence.
+ * Reurn: 0 if sucessful.
+ */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
