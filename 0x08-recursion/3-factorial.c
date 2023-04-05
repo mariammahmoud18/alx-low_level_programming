b11362c04f9f55652da546775ab271c11ac66ba4
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-05 15:43:24 -0700

    adding task3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,10 +9,11 @@
  * Return: factorial of n.
 */
 
-int factorial(int n){
+int factorial(int n)
+{
 if (n < 0)
 return (-1);
 if (n == 0 || n == 1)
-return 1;
+return (1);
 return (n * factorial(n - 1));
 }
