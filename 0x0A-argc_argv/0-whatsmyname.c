c8b0fe0a8339186e0bf44c63e390f9334db399f3
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-13 12:00:21 -0700

    adding first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+/**
+ * main - print the name of the file followed by a new line.
+ * @argc: number of arguments passed to the file.
+ * @argv: array of string of arguments.
+ * Return: 0 if successful.
+*/
+
+int main(int agrc, char *argv[])
+{
+    printf("./%s\n", argv[0]);
+    return (0);
+}
