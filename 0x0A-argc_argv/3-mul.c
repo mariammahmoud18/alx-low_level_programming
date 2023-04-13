b27dd85afab40a098bf81ddb00b190ded9442588
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-13 12:24:04 -0700

    adding fourth task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,14 +12,14 @@
 
 int main(int argc, char *argv[])
 {
-if (argc == 1)
+if (argc <= 2)
 {
 printf("Error\n");
 return (1);
 }
 int num1 = atoi(argv[1]);
 int num2 = atoi(argv[2]);
-int sum = num1 + num2;
+int sum = num1 * num2;
 
 printf("%d\n", sum);
 return (0);
