baf49c8a6cd8442ee98f76fe4f79f14a7503c6d2
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-23 16:37:19 -0700

    adding task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,7 +16,7 @@ int i;
 int j;
 if (width < 0 || height < 0)
 return (NULL);
-arr = (int **)malloc(height * sizeof(int*));
+arr = (int **)malloc(height * sizeof(int *));
 for (i = 0; i < height; i++)
 arr[i] = (int *)malloc(width * sizeof(int));
 if (arr == NULL)
