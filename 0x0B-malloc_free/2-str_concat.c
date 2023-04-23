4483847403bebe94e252c37b4a52f6a5f0260a10
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-23 16:06:03 -0700

    adding task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,11 +15,18 @@ char *str_concat(char *s1, char *s2)
 char *s;
 int i;
 int j = 0;
-int len1 = strlen(s1);
-int len2 = strlen(s2);
+int len1;
+int len2;
+if (s1 == NULL)
+len1 = 0;
+else
+len1 = strlen(s1);
+if(s2 == NULL)
+len2 = 0;
+else
+len2 = strlen(s2);
 int size = len1 + len2;
-
-s = malloc(sizeof(char)*(size + 1));
+s = malloc(sizeof(char) * (size + 1));
 if (s == NULL)
 return (NULL);
 for (i = 0; i < len1; i++)
