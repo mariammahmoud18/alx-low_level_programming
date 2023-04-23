594ffb2c72149ecd1c04d2573d947fc4cbd9fecb
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-23 15:35:12 -0700

    adding task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 char *s;
 unsigned int i;
 s = malloc(size);
-if (size == 0)
+if (size == 0 || s == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
 {
