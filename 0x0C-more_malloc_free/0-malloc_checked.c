f675c29c454d844dbcc2178b4c50e5dbd76acbb4
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-23 17:58:20 -0700

    adding task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * _malloc_checked - duplicate a string.
+ * @b: string to be dublicated.
+ * Return: pointer.
+*/
+void *malloc_checked(unsigned int b)
+{
+int *ptr;
+ptr = b;
+if (b == NULL)
+return (98);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Dealing with more malloc and free
