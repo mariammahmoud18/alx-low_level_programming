311a126d5c85dbe222acf3e9ed69971fb52134ff
mariammahmoud18 <mariammahmoud62001@gmail.com>
2023-04-23 18:15:41 -0700

    adding task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,11 +12,10 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ptr;
-int i;
-int size;
-int j = 0;
-int len1 = strlen(s1);
-int len2 = strlen(s2);
+unsigned int i;
+unsigned int j = 0;
+unsigned int len1 = strlen(s1);
+unsigned int len2 = strlen(s2);
 if (n > len2)
 n = len2;
 ptr = malloc(sizeof(char) * (len1 + n));
@@ -32,4 +31,4 @@ ptr[i] = s2[j];
 j++;
 }
 return (ptr);
-}
+)
